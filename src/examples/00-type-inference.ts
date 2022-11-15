029e28d963d8a79a3e5cfaa9ba87173e876af021
Alexander M. Turek <me@derrabus.de>
2022-11-15 11:07:42 +0100

    Example: type inference

---
+++ src/examples/00-type-inference.ts
@@ -0,0 +1,22 @@
+/**
+ * Cheat sheet: type inference
+ */
+
+// TS will infer a and b as type 'number'
+let a = 1;
+let b = 2;
+
+console.log(a, b);
+
+// This won't compile
+// b = 'foo';
+
+// TS will infer an array of number
+let myLuckyNumbers = [1, 4, 7, 42];
+myLuckyNumbers.push(43);
+// This won't compile either
+// myLuckyNumbers.push('foo');
+
+console.log(myLuckyNumbers);
+
+export {};
